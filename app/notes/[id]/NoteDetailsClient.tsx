ffddea1d168927fd8cbe195988aa71e8385ec788
RomanFS76 <36919661+RomanFS76@users.noreply.github.com>
2026-01-03 18:40:28 +0200

    feat: add HydrationBoundary for React Query cache hydration

---
+++ app/notes/[id]/NoteDetailsClient.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NoteDetailsClient = () => {
+  return (
+    <div>NoteDetailsClient111</div>
+  )
+}
+
+export default NoteDetailsClient
--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,12 +1,30 @@
+import { fetchNoteById } from '@/lib/api';
+import NoteDetailsClient from './NoteDetailsClient';
+import {
+  QueryClient,
+  HydrationBoundary,
+  dehydrate,
+} from '@tanstack/react-query';
+
 type Props = {
   params: Promise<{ id: string }>;
 };
 
 const NoteDetails = async ({ params }: Props) => {
   const { id } = await params;
-  console.log('note id:', id);
 
-  return <div>NoteDetails</div>;
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ['notes', id],
+    queryFn: () => fetchNoteById(id),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NoteDetailsClient />
+    </HydrationBoundary>
+  );
 };
 
 export default NoteDetails;
